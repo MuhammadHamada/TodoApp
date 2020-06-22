ebfce23e372b284ff5ae76b0ac3b595e9511165d
MuhammadHamada <homshamada@gmail.com>
2020-06-22 22:18:09 +0200

    Update the schema by adding new model (todolists)

--- app.py
+++ app.py
@@ -14,9 +14,16 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
         return f'<Todo ID: {self.id}, description: {self.description}>'
+
+class TodoList(db.Model):
+    __tablename__ = "todolists"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
 
 
 @app.route('/todos/create', methods = ['POST'])
